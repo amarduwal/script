a9ff4c5f4fc0e29fd875413f87440cb600f45467
alizduwal <duwal_amar2006@yahoo.com>
2019-03-29 11:22:15 +0545

    Rails installation script for ubuntu

---
+++ rails-installation-script.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Run this command to execute this file from local
+# bash < <(curl -sL https://raw.github.com/amarduwal/script/master/rails-installation-script.sh)
+
+if
+  [[ "${USER:-}" == "root" ]]
+then
+  echo "This script works only with normal user, it wont work with root, please log in as normal user and try again." >&2
+  exit 1
+fi
+
+# Check for existence of curl
+if 
+  [ ! -x /usr/bin/curl ]
+then
+  sudo apt-get install curl
+fi
+
+set -e
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+echo "Updates packages. Asks for your password."
+sudo apt-get update -y
+
+echo "Installs packages. Give your password when asked."
+sudo apt-get --ignore-missing install build-essential git-core curl openssl libssl-dev libcurl4-openssl-dev zlib1g zlib1g-dev libreadline6-dev libyaml-dev libsqlite3-dev libsqlite3-0 sqlite3 libxml2-dev libxslt1-dev libffi-dev software-properties-common libgdm-dev libncurses5-dev automake autoconf libtool bison postgresql postgresql-contrib libpq-dev pgadmin3 libc6-dev nodejs -y
+
+echo "Installs ImageMagick for image processing"
+sudo apt-get install imagemagick --fix-missing -y
+
+echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
+# Retrieve the GPG key.
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+
+echo "Installs Ruby"
+rvm install 2.6.0
+rvm use 2.6.0 --default
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+gem install bundler
+gem install rails
+
+echo -e "\n- - - - - -\n"
+echo -e "Now we are going to print some information to check that everything is done:\n"
+
+
+echo -n "Should be sqlite 3.8.1 or higher: sqlite "
+sqlite3 --version
+echo -n "Should be rvm 1.26.11 or higher:         "
+rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-11
+echo -n "Should be ruby 2.6.0:                "
+ruby -v | cut -d " " -f 2
+echo -n "Should be Rails 5.2.1 or higher:         "
+rails -v
+echo -e "\n- - - - - -\n"
+
+echo "If the versions match, everything is installed correctly. If the versions
+don't match or errors are shown, something went wrong with the automated process
+and we will help you do the installation the manual way.
+
+Congrats!
+
+Make sure that all works well by running the application generator command:
+    $ rails new numbernine
+
+If you encounter the message:
+    The program 'rails' is currently not installed.
+
+It is just a hiccup with the shell, solutions:                                   
+    $ source ~/.rvm/scripts/rvm
+    Allow login shell, example http://rvm.io/integration/gnome-terminal/"
